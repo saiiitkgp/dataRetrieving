22508e0ba4a5038453753feb32897388450f620e
chandra kiran <511chandrakiran@gmail.com>
2020-03-10 00:48:17 +0530

    updated

--- extract1.py
+++ extract1.py
@@ -1,25 +1,39 @@
-import requests, sys, os, json, re
+import requests, sys, os, json, time
+
+key = "b11ad4995e5003463aa7f750d36f0cb2"
+latitude = "42.3601"
+longitude = "-71.0589"
+time = int(time.time())
+time = str(time)
 
 def takeInputs():
 	try:
-		latitude = sys.argv[1]
-		longitude = sys.argv[2]
+		latitude = str(sys.argv[1])
+		longitude = str(sys.argv[2])
+		tFlag = sys.argv[3]
+		if tFlag == "-t":
+			time = str(sys.argv[4])
 	except:
 		print("E R R O R !! arguments were missing")
 		sys.exit(1)
 
-latitude = "42.3601"
-longitude = "-71.0589"
-#takeInputs()
-key = "b11ad4995e5003463aa7f750d36f0cb2"
-time = ""
+def loadJson(jsonName):
+	try:
+		if os.path.isfile(jsonName):
+			with open(jsonName) as js:
+				dit=json.load(js)
+			return dit
+		else:
+			print("E R R O R !! "+jsonName+" file is not present in the directory")
+	except Exception as e:
+			print(e)
 
-api = "https://api.darksky.net/forecast/+"key+"/"+latitude+","+longitude+","+time+"?exclude=currently,flags"
-print(api)
-#from sys import argv
-response = requests.get(api)
-print(response.status_code)
-print(response.json())
-print(args.tm)
-with open ('mydata.json' ,'w') as f:
-    json.dump(data,f)
+
+takeInputs()
+
+api = "https://api.darksky.net/forecast/"+key+"/"+latitude+","+longitude+","+time+"?exclude=currently,daily,flags"
+response = requests.get(api).json()
+with open ('newData.json' ,'w') as f:
+    json.dump(response,f, indent=3, sort_keys=False)
+
+#loaded = loadJson("newData.json")
