ac9dde40513bc5852a679ace1001f08844b7392f
chandra kiran <511chandrakiran@gmail.com>
2020-03-09 21:09:10 +0530

    changed

---
+++ extract1.py
@@ -0,0 +1,27 @@
+import requests, sys, os, json, re
+
+def takeInputs():
+	try:
+		latitude = sys.argv[1]
+		longitude = sys.argv[2]
+	except:
+		print("E R R O R !! arguments were missing")
+		sys.exit(1)
+
+latitude = "42.3601"
+longitude = "-71.0589"
+time = ""
+#takeInputs()
+
+key = "b11ad4995e5003463aa7f750d36f0cb2"
+
+
+api = "https://api.darksky.net/forecast/+"key+"/"+latitude+","+longitude+","+args.tm+"?exclude=currently,flags"
+print(api)
+#from sys import argv
+response = requests.get(api)
+print(response.status_code)
+print(response.json())
+print(args.tm)
+with open ('mydata.json' ,'w') as f:
+    json.dump(data,f)
